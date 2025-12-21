55adcda07b44cc5ccf936de33c6d6d94862e7b8c
natively.dev <api@natively.dev>
2025-12-21 18:11:24 +0000

    fix: Enforce npm and prevent pnpm/Yarn usage with config files

---
+++ .pnpmfile.cjs
@@ -0,0 +1,5 @@
+
+// This project is configured to use npm, not pnpm
+// If you see this error, please run: npm install
+
+throw new Error('This project is configured to use npm, not pnpm. Please run: npm install');
