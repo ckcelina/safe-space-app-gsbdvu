4b53c3e8792e10b8e0999599b2510d5e90af079e
natively.dev <api@natively.dev>
2025-12-21 18:13:07 +0000

    fix: Remove .pnpmfile.cjs to prevent pnpm execution and build errors

--- .pnpmfile.cjs
+++
@@ -1,5 +0,0 @@
-
-// This project is configured to use npm, not pnpm
-// If you see this error, please run: npm install
-
-throw new Error('This project is configured to use npm, not pnpm. Please run: npm install');
