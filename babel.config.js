3ad9ce99a4f1bd9667ef188a21ddc9541b1a22c6
natively.dev <api@natively.dev>
2026-01-10 17:34:19 +0000

    build: Replace deprecated Babel plugin and add missing alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+
 module.exports = function (api) {
   api.cache(true);
 
@@ -35,11 +36,12 @@ module.exports = function (api) {
             "@hooks": "./hooks",
             "@types": "./types",
             "@contexts": "./contexts",
+            "@lib": "./lib",
           },
         },
       ],
       ...EDITABLE_COMPONENTS,
-      "@babel/plugin-proposal-export-namespace-from",
+      "@babel/plugin-transform-export-namespace-from",
       "react-native-worklets/plugin", // react-native-worklets/plugin must be listed last!
     ],
   };
